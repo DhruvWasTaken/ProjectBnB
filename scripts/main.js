9d154eaae0c5de990ad83183b5115a1a2a89f806
DhruvWasTaken <62846058+DhruvWasTaken@users.noreply.github.com>
2020-07-03 12:07:39 +0530

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+function search_blog() { 
+    let input = document.getElementById('searchbar').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('card h-100'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="card-body";                  
+        } 
+    } 
+} 
+
+
+
+
+
+
